c0acc8c763be1a1a66a67678dea9f649f1a67bd2
Denis Avakov <denis.avakov@pm.me>
2022-04-08 15:37:34 +0300

    refactor: remove unused vars

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,6 @@
-import vars from './vars';
-
 export async function handleRequest(request: Request): Promise<Response> {
-  console.log('_ vars', vars.EXAMPLE_ENVIRONMENT_VARIABLE);
-  console.log('_ kv', await FOSSABOT.get('EXAMPLE_KV'));
+  console.log('_ vars', EXAMPLE_ENVIRONMENT_VARIABLE);
+  console.log('_ kv', await EXAMPLE_KV_NAMESPACE.get('EXAMPLE_VARIABLE'));
 
   return new Response(`Request method: ${request.method}`);
 }
