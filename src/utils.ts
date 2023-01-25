2ff38ab61bc6066cb440fb37d1a194bc677afe2d
Denis Avakov <denis.avakov@pm.me>
2023-01-26 00:48:52 +0200

    fix: combine multiple imports from 'hono/utils/http-status'

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,5 @@
-import { getStatusText } from 'hono/utils/http-status';
-
+import { getStatusText, type StatusCode } from 'hono/utils/http-status';
 import { type Context } from 'hono';
-import { type StatusCode } from 'hono/utils/http-status';
 
 export const createError = (context: Context, status: StatusCode, message?: string) => {
   return context.json(
