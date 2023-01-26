18ca41770659a0fdae43e3ece53a0abd00a03abb
Denis Avakov <denis.avakov@pm.me>
2023-01-26 03:00:12 +0200

    refactor: simplify the boilerplate

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,10 @@ import { getStatusText, type StatusCode } from 'hono/utils/http-status';
 import { type Context } from 'hono';
 
 export const createError = (context: Context, status: StatusCode, message?: string) => {
-  return context.json(
-    {
-      status,
-      statusText: message ?? getStatusText(status)
-    },
-    status
-  );
+  const body = {
+    status,
+    statusText: message ?? getStatusText(status)
+  };
+
+  return context.json(body, status);
 };
